d5d3712b5fd5c12d9a13c52340b61f1b6c63cc55
Tetsuo Kiso <tetsuokiso9@gmail.com>
2014-06-13 02:16:13 +0900

    Delete unused functions; Fix the benchmarks for Tokenize

--- pegasos/tokenize.go
+++ pegasos/tokenize.go
@@ -7,57 +7,10 @@
 package pegasos
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"strconv"
 )
-
-var ErrSyntax = errors.New("invalid syntax")
-
-// NumError same as strconv.atoi.go
-type NumError struct {
-	Func string
-	Num  string
-	Err  error
-}
-
-func (e *NumError) Error() string {
-	return "tokenize." + e.Func + ": " + e.Num + ": " + e.Err.Error()
-}
-
-func syntaxError(fn, str string) *NumError {
-	return &NumError{fn, str, ErrSyntax}
-}
-
-func tokenizeNode(s string) (v *Node, err error) {
-	const fnTokenizeNode = "tokenizeNode"
-	if len(s) == 0 {
-		return nil, syntaxError(fnTokenizeNode, "empty string")
-	}
-
-	sep := uint8(':')
-	id := 0
-	val := 0.0
-
-	for i := 0; i < len(s); i++ {
-		if s[i] != sep {
-			continue
-		}
-		id, err = strconv.Atoi(s[0:i])
-		if err != nil {
-			goto Error
-		}
-		val, err = strconv.ParseFloat(s[i+1:], 64)
-		if err != nil {
-			goto Error
-		}
-		return &Node{id, val}, nil
-	}
-
-Error:
-	return nil, syntaxError(fnTokenizeNode, s)
-}
 
 func Tokenize(line string, lineNum int) (x Example, maxId int) {
 	ptr := 0
